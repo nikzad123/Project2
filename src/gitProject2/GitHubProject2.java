87df0976ffe671e4b615fde15e901c178d35d4fa
mohammadpanahnikzad <mohammadpanahnikzad@localhost>
2023-04-06 00:27:02 -0400

    GitHub Test 2....

---
+++ src/gitProject2/GitHubProject2.java
@@ -0,0 +1,13 @@
+package gitProject2;
+
+public class GitHubProject2 {
+	
+	public static void main(String args []) {
+		
+		System.out.println("GitHub Project 2...");
+		
+		System.out.println("Test 2....");
+		
+	}
+
+}
